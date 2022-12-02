92faeb6e4e6f29290688b1828da8766446cd28a3
Gaston Avogadro <gaston.avogadro@gmail.com>
2022-12-02 11:19:29 -0300

    test bg animated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,11 @@
 import { Link } from 'react-scroll';
+import BgAnimated from '../BgAnimated/BgAnimated';
 import './Home.css';
 
 const Home = () => {
     return (
         <section className="homeContainer" id="home">
+            <BgAnimated />
             <div className="heroSection">
                 <p>Hi, I am</p>
                 <h1>Gaston Avogadro</h1>
